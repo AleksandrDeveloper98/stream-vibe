832a25cf421b3096493f89033aa36c57cdbf00fb
Aleksandr Belov <110898Belov@mail.ru>
2026-01-27 17:52:39 +0400

    dev: main page 6.7/6.13

---
+++ src/sections/Questions/Questions.jsx
@@ -0,0 +1,33 @@
+import Section from "@/layouts/Section"
+import Button from "@/components/Button"
+import "./Questions.scss"
+import AccordionGroup from "@/components/AccordionGroup"
+
+const Questions = () => {
+  const questionsItems = [
+    "What is StreamVibe?",
+    "How much does StreamVibe cost?",
+    "What content is available on StreamVibe?",
+    "How can I watch StreamVibe?",
+    "How do I sign up for StreamVibe?",
+    "What is the StreamVibe free trial?",
+    "How do I contact StreamVibe customer support?",
+    "What are the StreamVibe payment methods?",
+  ]
+
+  return (
+    <Section
+      title="Frequently Asked Questions"
+      titleId="questions-title"
+      description="Got questions? We've got answers! Check out our FAQ section to find answers to the most common questions about StreamVibe."
+      className="questions"
+      actions={<Button href="/support" label="Ask a Question" />}
+    >
+      <AccordionGroup columns={2}>
+        {questionsItems}
+      </AccordionGroup>
+    </Section>
+  )
+}
+
+export default Questions
